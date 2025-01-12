c3e48028e208762678fc2ed45023651e63247e7b
Thanapat Nawang <thanapattt.m@gmail.com>
2025-01-12 20:38:24 +0700

    Setup: Theme

--- app/src/main/java/com/thanapat/mybio/ui/theme/Theme.kt
+++ app/src/main/java/com/thanapat/mybio/ui/theme/Theme.kt
@@ -11,16 +11,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+private val AppColorTheme = lightColorScheme(
+    primary = Blue,
+    background = WhiteBG
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -40,18 +33,9 @@ fun MyBioTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
-    }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = AppColorTheme,
         typography = Typography,
         content = content
     )
